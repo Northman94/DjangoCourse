da1e058a18996ebbc5f3b9bd05237429236c5dab
Northman94 <Zhandarm2000@gmail.com>
2022-10-11 23:29:01 +0300

    Home page creation + transfer to view

--- Edenthrough/journal/views.py
+++ Edenthrough/journal/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Creating a View:
+
+def home(request):
+    return HttpResponse("Home Page.")
+
+
+def register(request):
+    return HttpResponse("Welcome to Registration Page.")
+
