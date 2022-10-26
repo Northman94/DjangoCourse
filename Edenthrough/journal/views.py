341366f6620b1e18442a194bafe3e78546bed3c3
Northman94 <Zhandarm2000@gmail.com>
2022-10-26 23:25:42 +0300

    Rendering Data into Template - Part 3

--- Edenthrough/journal/views.py
+++ Edenthrough/journal/views.py
@@ -2,18 +2,35 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-# Creating a View:
+clientList = [
+
+    {
+        'id' : '1',
+        'name' : 'Chris Mule',
+        'profession' : 'Musician',
+    },
+
+    {
+        'id' : '2',
+        'name' : 'Sarah',
+        'profession' : 'Carpenter',
+    },
+]
+
+
+
+
+
+def register(request):
+
+    return render(request, 'register.html')
+
+
 
 def home(request):
     #return HttpResponse("Home Page.")
 
-    first_name = "John"
-
-    context = {'name':first_name}
+    context = {'clients' : clientList}
 
     return render(request, 'index.html', context)
-
-
-def register(request):
-    return render(request, 'register.html')
 
