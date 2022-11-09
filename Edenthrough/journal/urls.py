f6152681c8c88223035dea32e22cbec8427a8b17
Northman94 <Zhandarm2000@gmail.com>
2022-11-09 21:56:32 +0200

    cleaning everything related to register.html

--- Edenthrough/journal/urls.py
+++ Edenthrough/journal/urls.py
@@ -4,7 +4,5 @@ from . import views
 urlpatterns = [
 
     path("", views.home),
-
-    path("register/", views.register),
 ]
 
--- Edenthrough/journal/views.py
+++ Edenthrough/journal/views.py
@@ -2,34 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-clientList = [
-
-    {
-        'id' : '1',
-        'name' : 'Chris Mule',
-        'profession' : 'Musician',
-    },
-
-    {
-        'id' : '2',
-        'name' : 'Sarah',
-        'profession' : 'Carpenter',
-    },
-]
-
-
-
-
-def register(request):
-
-    return render(request, 'register.html')
-
-
 
 def home(request):
     #return HttpResponse("Home Page.")
 
-    context = {'clients' : clientList}
-
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
 
